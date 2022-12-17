8f2298d88a79d19abd0acae1bf56ebdacac897ca
Meghdeep Chudasma <110728369+meghdeep18@users.noreply.github.com>
2022-12-17 20:45:11 +0530

    types of operators

--- operators.js
+++ operators.js
@@ -1,4 +1,6 @@
-// ===>  arethicmetic operators
-// ===>  logical operators 
-// ===>  comparison operators --> <,>,=,==,===,
-//
+// ===>  1  arethicmetic operators
+// ===>  2  Assignment Operators
+// ===>  3   Comparison Operators
+// ===>  4   Logical Operators
+// ===>  5   Conditional Operators
+// ===>  6   Type Operators
